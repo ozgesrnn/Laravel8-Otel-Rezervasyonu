76076781199f11440d3b21b32916cc4f12a95891
ozgesrnn <ozgesrnn@gmail.com>
2020-12-27 18:52:17 +0300

    index changed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,25 +1,67 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+namespace App\Http\Controllers;
+
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
     //
     public function index()
     {
-        return view('admin.index');
+        return view('home.index');
 
     }
-/*
+    public function login()
+    {
+        return view('admin.login');
+    }
+
+    public function logincheck(Request $request)
+    {
+
+        if ($request->isMethod('post'))
+        {
+            $credentials = $request->only('email', 'password');
+            if (Auth::attempt($credentials)) {
+                $request->session()->regenerate();
+
+                return redirect()->intended('admin.index');
+
+            }
+            return back()->withErrors([
+                'email' => 'The provided credentials do not match our records.',
+
+            ]);
+        }
+        else
+        {
+            return view('admin.login');
+        }
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+        $request->session()->regenerateToken();
+        return redirect('/');
+
+    }
+
+
+
+
     public function aboutus()
     {
         return view('home.about');
 
     }
 
-    /*
+/*
     public function test($id,$name)
     {
         $data['id']=$id;
